b29f26f61ad3080983ebca70c75d62aeb2442078
hangz007 <hangz00749@163.com>
2019-04-09 00:05:08 +0800

    整合Hystrix

--- product-service/src/main/java/cn/clouddemo/ProductServiceApplication.java
+++ product-service/src/main/java/cn/clouddemo/ProductServiceApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+
 @EnableCircuitBreaker
 @EnableEurekaClient
 @SpringBootApplication
